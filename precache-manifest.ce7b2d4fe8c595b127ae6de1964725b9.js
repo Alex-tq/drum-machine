7209eabc93e39a2b70b500f38c7c61edc7ddb10c
unknown <alextq3036@gmail.com>
2021-10-20 16:40:50 -0400

    Updates

---
+++ precache-manifest.ce7b2d4fe8c595b127ae6de1964725b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43fc52b3a3e0fd9d67db3b695dc7b4d6",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "77b9b10a3463fdf4253b",
+    "url": "/drum-machine/static/css/main.ea49af42.chunk.css"
+  },
+  {
+    "revision": "98bf0fd87cf6cebead5e",
+    "url": "/drum-machine/static/js/2.af9fcefb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/drum-machine/static/js/2.af9fcefb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77b9b10a3463fdf4253b",
+    "url": "/drum-machine/static/js/main.ecc9b722.chunk.js"
+  },
+  {
+    "revision": "8812de4e5e4091b01bac",
+    "url": "/drum-machine/static/js/runtime-main.c8eb23b9.js"
+  }
+]);

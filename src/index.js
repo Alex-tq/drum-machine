c6d9d6127e1a3a1b5fb59999129d8549e91a8a21
unknown <alextq3036@gmail.com>
2021-10-20 16:20:58 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DrumMachine from './DrumMachine';
+import './index.scss';
+
+
+ReactDOM.render(
+    <DrumMachine/>,
+    document.getElementById('root')
+    )
